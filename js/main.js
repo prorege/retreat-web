baddac4a4c78254d8dc652aa04f343b4b1bbc8f9
eden <yangsehyuk1117@gmail.com>
2026-02-26 14:15:25 +0900

    0

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ import { showEmergency } from './showEmergency.js';
 import { showResolution } from './showResolution.js';
 import { showFasting } from './showFasting.js';
 import { showAllFasting } from './showAllFasting.js';
-import { typingText } from './typingText.js';
 import { loadCSV } from './utils/loadCSV.js';
 import { showLinktree } from './showLinktree.js';
 import { showWashClean } from './showWashClean.js';
@@ -25,7 +24,6 @@ export const appHandlers = {
   showResolution,
   showFasting,
   showAllFasting,
-  typingText,
   showLinktree,
   showWashClean
 };
@@ -36,5 +34,4 @@ window.appHandlers = appHandlers;
 // 초기 로딩 시 CSV 데이터 미리 불러오기
 window.addEventListener('DOMContentLoaded', async () => {
   await loadCSV(); // ✅ 참가자 CSV 미리 로드 & 캐싱
-  typingText();
 });
